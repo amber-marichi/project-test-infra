1b70628ddb806b3d35ae27182ae429cbc6a11176
Max Bilokur <max@pop-os.localdomain>
2024-06-12 17:11:58 +0300

    edit: script file bad lint

--- src/find_players_with_zero_or_one_losses.py
+++ src/find_players_with_zero_or_one_losses.py
@@ -1,8 +1,7 @@
 '''
 2225. Find Players With Zero or One Losses
 
-You are given an integer array matches where matches[i] = [winneri, loseri]
-indicates that the player winneri defeated player loseri in a match.
+You are given an integer array matches where matches[i] = [winneri, loseri] indicates that the player winneri defeated player loseri in a match.
 
 Return a list answer of size 2 where:
 
@@ -13,17 +12,14 @@ The values in the two lists should be returned in increasing order.
 
 Note:
 You should only consider the players that have played at least one match.
-The testcases will be generated such that no two matches
-will have the same outcome.
+The testcases will be generated such that no two matches will have the same outcome.
 '''
 
-
 def find_winners(matches: list[list[int]]) -> list[list[int]]:
-    'Solution for 2225. Find Players With Zero or One Losses'
     winners, onnes, outs = set(), set(), set()
     for win, lost in matches:
         if win not in outs and win not in onnes:
-            winners.add(win)
+                winners.add(win)
         if lost in outs:
             continue
         if lost in winners:
