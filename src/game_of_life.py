6021ea2beafbb021f2ccdc3fae3866007635c9f4
Max Bilokur <max@pop-os.localdomain>
2024-06-13 10:18:05 +0300

    edit: bad lint game

--- src/game_of_life.py
+++ src/game_of_life.py
@@ -1,5 +1,5 @@
 class GameOfLife:
 
     def game(self, filename_input: str, filename_output: str) -> None:
-        with open(filename_input, 'r') as fr, open(filename_output, 'w') as fw:
-            fw.write(fr.read())
+            with open(filename_input, 'r') as fr, open(filename_output, 'w') as fw:
+                fw.write(fr.read())
